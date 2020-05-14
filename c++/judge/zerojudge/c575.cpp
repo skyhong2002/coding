db6e0b7056966ab2dc389e65cb24dfa1d771fbb6
Sky Hong <sky9109@gmail.com>
2020-05-14 17:21:19 +0800

    add c575.cpp

---
+++ c++/judge/zerojudge/c575.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool test(int N, int K, int P[], int D){
+    int n=0;
+    for(int j=0; j<K; i++){
+        int ns=n;
+        while(D<P[n]-P[ns]){
+            n++;
+            if(n>=N) return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int N, K, D;
+    int P[500002];
+    while(cin >> N >> K){
+        for(int i=0; i<N; i++) cin >> P[i];
+        sort(P; P+N);
+
+        int dl=0, dr=N;
+        while(dl==dr-1){
+            if(test(N, K, P, (dl+dr)/2)) dl = (dl+dr)/2;
+            else dr = (dl+dr)/2;
+        }
+        cout << dl << "\n";
+    }
+}
