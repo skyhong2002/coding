be781c0311829947fa43723ec544b27547945b1e
Sky Hong <sky9109@gmail.com>
2020-08-19 20:56:37 +0800

    upload

--- c++/judge/zerojudge/b304.cpp
+++ c++/judge/zerojudge/b304.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 int main(){
 
-
+    string a;
+    int cnt;
+    cin >> cnt;
+    
+    for(int i=0; i<cnt; i++){
+        stack<char> stk;
+        bool b = true;
+        cin >> a;
+        for(char c : a){
+            if (a=='(') stk.push('(');
+            else if(a=='[') stk.push('[');
+            else if(a==')'){
+                if(stk.top()=='(')stk.pop();
+                else b = false;
+            }
+            else{
+                if(stk.top()=='[')stk.pop();
+                else b = false;
+            }
+        }
+        if(!stk.empty()) b = false;
+        cout << (b ? "Yes" : "No");
+    }
     return 0;
 }
